6e8345d0c77c4e7f2c6482c983e760fe0decfbbb
yeupdong <yeupdong@gmail.com>
2022-10-19 12:51:45 +0900

    day02

---
+++ day02.py
@@ -0,0 +1,30 @@
+'''
+round -> 소수점 기능
+
+flt = 123.567
+print(flt)
+print('round(flt):',round(flt))
+print('round(flt,1):',round(flt,1))
+print('round(flt,2):',round(flt,2))
+
+
+
+
+'''
+
+flt = 123.567
+print(flt)
+print('round(flt):',round(flt))
+print('round(flt,1):',round(flt,1))
+print('round(flt,2):',round(flt,2))
+print('\t')
+print('\t')
+a = 37/11
+print('Min:',round(a,2))
+print('\t')
+print('\t')
+b = (260*13)+500.23
+print('Height:',round(b,3))
+
+
+
